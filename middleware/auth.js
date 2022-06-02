0712338821b332a50feb06ba2b8ef114ac3b8ccf
Aredon <alexredon75@gmail.com>
2022-06-02 10:45:56 +0200

    Modif de page d'erreur

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,7 +1,7 @@
 export const authMiddleware = (req, res, next) => {
 	if (!req.session.auth) {
 		console.log("non connecte");
-		return res.send("NON CONNECTE");
+		return res.render("errorpage", {});
 	}
 	console.log("connecté");
 
